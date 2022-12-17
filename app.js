5db0c9b796599fa3fbe48ef65facf2d20cb7aac0
isaackam <isaacgun@outlook.com>
2022-12-17 09:46:40 +0800

    feat: add winson.add safelog

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const Koa = require('koa');
+const safelog = require('./src/utils/log');
 const app = new Koa();
 
 app.use(async ctx => {
+  safelog.info(() => [
+    "123, %j", aaaa
+  ]);
+  safelog.info(
+    "123, %j", { age: 123444 });
   ctx.body = 'Hello World';
 });
 
---
+++ src/utils/log.js
@@ -0,0 +1,49 @@
+const winston = require("winston");
+const { combine, timestamp, printf, splat } = winston.format;
+const logger = winston.createLogger({
+  format: combine(
+    splat(),
+    timestamp(),
+    printf(({ level, message, timestamp }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    // 输出到 stdout（控制台）
+    new winston.transports.Console(),
+    // 将日志写入文件
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+const safeloggerFactory = (logger) => {
+  const logFactory = (level) => (...rest) => {
+    try {
+      let params = rest;
+      if (rest.length === 1 && Object.prototype.toString.call(rest[0]) === "[object Function]") {
+        params = rest[0]();
+        console.info(params);
+      }
+      logger[level](...params); 
+    } catch (error) {
+      // log err
+      logger.warn("[logger error] [level:%s] errMsg:%s", level, error.message);
+    }
+  };
+  return {
+    error: logFactory("error"),
+    info: logFactory("info"),
+    warn: logFactory("warn")
+  };
+};
+
+const safelog = safeloggerFactory(logger);
+
+safelog.error(() => ['Hello again distributed logs, %j', (() => {
+  if (1) throw new Error("test")
+  return { age: 12 }
+})()])
+safelog.info(() => ['Hello again distributed logs']);
+safelog.warn(() => ['Hello again distributed logs']);
+
+module.exports = safelog;
