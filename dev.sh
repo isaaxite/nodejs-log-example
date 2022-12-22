b04fef7a529d65384c862b8ce1a8267c985658ab
isaackam <isaacgun@outlook.com>
2022-12-23 04:13:17 +0800

    update workspace

--- dev.sh
+++ dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run --rm -it \
   -p 3000:3000 \
-  -v `pwd`:/workspace \
-  -w /workspace \
+  -v `pwd`:/data/svr \
+  -w /data/svr \
   -e NODE_ENV="development" \
   nodejs_log_example/svr:latest \
   /bin/bash
